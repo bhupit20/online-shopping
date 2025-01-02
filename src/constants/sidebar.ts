d513f7c1a21c341f1b8eae3f1a566c79352e6ca9
bhupit20 <bhupindersinght20@gmail.com>
2025-01-02 17:00:12 +0530

    pushing

---
+++ src/constants/sidebar.ts
@@ -0,0 +1,12 @@
+import { ROUTES_ENUM } from "@/enums/routes";
+
+export const NAVIGATION_ITEMS = [
+  {
+    title: "Products",
+    path: ROUTES_ENUM.PRODUCTS,
+  },
+  {
+    title: "Favorites",
+    path: ROUTES_ENUM.FAVORITES,
+  },
+];
